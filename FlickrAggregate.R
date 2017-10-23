9896e3b594229362a9e5600053b6dd259e4299ab
Francesca Mancini <r03fm14@abdn.ac.uk>
2017-10-23 17:24:04 +0100

    Visotor days - delete multiple photo from same user on same day and country

--- FlickrAggregate.R
+++ FlickrAggregate.R
@@ -9,7 +9,7 @@
 library(data.table)
 library(sp)
 library(rworldmap)
-library(ggplot2)
+library(lubridate)
 
 # download a world map at a coarse resolution
 world <- getMap(resolution = "coarse")
@@ -46,5 +46,32 @@ names(flickr)[4] <- "Country"
 
 ##################################################
 
+flickr <- fread("C:\\Users\\r03fm14\\OneDrive - University of Aberdeen\\Data\\Flickr100M\\filteredData.txt",
+                colClasses = c("character", "character", "character", "character"), sep = " ", header = T)
+
+flickr$year<-year(ymd_hms(flickr$date))             #create variable year
+
+# some observations do not have a date
+# delete rows where date is null
+
+flickr <- flickr[-which(flickr$date=="null"),]
+
+summary(flickr$year)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 1    2008    2010    2010    2012    8981 
+
+# some of the dates were misreported
+# delete all those later than 2014
+flickr <- flickr[-which(flickr$year>2014),]
+
+# and all those earlier than 2004 (Flickr launch year)
+flickr <- flickr[-which(flickr$year<2004),]
+
+# divide date from time
+flickr$dateonly<-as.character(sapply(strsplit(flickr$date, "_"), "[[", 1))   
+# find multiple pictures from the same photographer in the same day and in the same country
+d<-duplicated(flickr[,c(2,3,4)])                        
+# eliminate duplicates
+flickr_VD<-flickr[-which(d=="TRUE"),]               
 
 
