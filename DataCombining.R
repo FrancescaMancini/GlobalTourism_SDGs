10646731989b1789c352028baa0af75bf1d6a1b4
Francesca Mancini <r03fm14@abdn.ac.uk>
2017-11-06 18:50:05 +0000

    Combine all datasets into one

--- DataCombining.R
+++ DataCombining.R
@@ -21,7 +21,7 @@ names(NFVD)[3] <- "NFVD"
 # United Nations Sustainable Development Goals Indicators for goals 8, 12, 14 and 15
 indicators <- read.table(paste(dataFilePath, "WBSDGindicators/SDG_IndicatorsData.txt", sep = ""), 
                          header = T, colClasses = c("factor", "factor", "character", "factor",
-                                                    rep("numeric", 28)), na.string = "..", sep = "\t")
+                                                    rep("numeric", 28)), na.string = "..", sep = "\t", quote="\"")
 # WTO data on number of doemstic guests in accommodations per country per year
 domestic_guests <- read.table(paste(dataFilePath, "UNWTO/Domestic_Accommodation_Guests.txt", sep = ""),
                               header = T, colClasses = c("numeric", "factor",rep("numeric", 21)),
@@ -106,7 +106,80 @@ NFVD_prop <- merge(NFVD, FVD, all = T)
 NFVD_prop$NFVD_prop <- (NFVD_prop$NFVD/NFVD_prop$FVD)
 write.table(NFVD_prop, paste(dataFilePath, "Flickr100M/NFVD_prop.txt", sep = ""), row.names = F)
 
+##############################################
 # combine tourism, flickr and indicators
+##############################################
+library(countrycode)
+library(plyr)
+
 tourism <- read.table(paste(dataFilePath, "UNWTO/WTOcombined.txt", sep = ""), header = T)
 flickr <- read.table(paste(dataFilePath, "Flickr100M/NFVD_prop.txt", sep = ""), header = T)
 indicators <- read.table(paste(dataFilePath, "WBSDGindicators/indicators_tidy.txt", sep = ""), header = T)
+
+# names of countries are different in the 3 datasets
+# need to transform into ISO codes
+# first make all lowercase
+tourism$COUNTRY <- as.factor(tolower(tourism$COUNTRY))
+indicators$Country_Name <- as.factor(tolower(indicators$Country_Name))
+flickr$country <- as.factor(tolower(flickr$country))
+
+# convert flickr country names into ISOcodes
+flickr$country_code <- countrycode(flickr$country, "country.name", "iso3c")
+# Warning message:
+#   In countrycode(flickr$country, "country.name", "iso3c") :
+#   Some values were not matched unambiguously: ashmore and cartier islands, indian ocean territories, kosovo, saint martin, siachen glacier
+
+# manually look for ISO codes and change them
+flickr$country_code[which(flickr$country == "ashmore and cartier islands")] <- "AUS"
+
+flickr$country_code[which(flickr$country == "indian ocean territories")] <- "IOT"
+
+flickr$country_code[which(flickr$country == "kosovo")] <- "XKX"
+
+flickr$country_code[which(flickr$country == "saint martin")] <- "MAF"
+
+flickr$country_code[which(flickr$country == "siachen glacier")] <- "IND"
+
+# now aggregate all columns with same country code and year
+flickr <- ddply(flickr, c("country_code", "year"), numcolwise(sum), na.rm = T)
+flickr$NFVD_prop <- flickr$NFVD/flickr$FVD
+
+
+# convert tourism country names into ISO3 codes
+tourism$country_code <- countrycode(tourism$COUNTRY, "country.name", "iso3c")
+# Warning message:
+#   In countrycode(tourism$COUNTRY, "country.name", "iso3c") :
+#   Some values were not matched unambiguously: bonaire, saba, sint eustatius
+
+# manually look for ISO codes and change them
+tourism$country_code[which(is.na(tourism$country_code) == TRUE)] <- "BES"
+
+# now aggregate all columns with same country code and year
+# need to treat NAs in different ways:
+# 1) as 0s when adding to a number, e.g. 1+NA=1
+# 2) as NA when adding to NA, e.g. Na+NA=NA
+# build a custom function for this
+# and apply it to rows with same country code and year
+plus <- function(x) {
+  if(all(is.na(x))){
+    c(x[0],NA)} else {
+      sum(x,na.rm = TRUE)}
+}
+
+tourism <- ddply(tourism, c("country_code", "year"), numcolwise(plus))
+
+# now merge datasets
+tourism_combined <- merge (tourism, flickr, by = c("country_code", "year"), all = TRUE)
+tourism_combined <- tourism_combined[,-c(3, 13, 14)]
+
+write.table(tourism_combined, paste(dataFilePath, "UNWTO/tourism_vars.txt", sep = ""),
+            sep = "\t", row.names = F)
+
+# reshape indicators dataframe from long to wide
+# so every indicator is a separate column
+indicators <- indicators[,-c(1,3)]
+indicators_wide <- spread(indicators, key = Series_Code, value = value)
+names(indicators_wide)[1] <- "country_code"
+combined <- merge (tourism_combined, indicators_wide, by = c("country_code", "year"), all = TRUE)
+
+write.table(combined, "combined.txt", sep = "\t", row.names = F)
